88063671f52d6f320acf93bfde0b692d34a5c666
bartlomiejsitek <bartlomiej.sitek0@gmail.com>
2025-10-21 09:01:20 +0000

    Com2_1.1

--- script.js
+++ script.js
@@ -15,8 +15,24 @@
   })
 
   cw1.addEventListener("click", function () {
-    //TODO
-  })
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+      method: 'GET'
+    })
+      .then(response => response.json())
+      .then(data => {
+        const posts = data; 
+        const listItems = posts.map(post => {
+          return `<li><strong>${post.title}</strong><br>${post.body}</li>`;
+        });
+
+        const html = `<ul>${listItems.join('')}</ul>`;
+        answer.innerHTML = html;
+      })
+      .catch(error => {
+        console.error(error);
+        answer.innerHTML = "Błąd podczas pobierania postów.";
+      });
+  });
 
   cw2.addEventListener("click", function () {
     //TODO
