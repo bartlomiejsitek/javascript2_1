95f6e43bb57b129e15890f55c0797cb05ed11e9f
bartlomiejsitek <bartlomiej.sitek0@gmail.com>
2025-10-21 09:04:28 +0000

    Com2_1.2

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-(function () {
+(function() {
   const example = document.getElementById('example')
   const cw1 = document.getElementById('cw1')
   const cw2 = document.getElementById('cw2')
   const cw3 = document.getElementById('cw3')
   const answer = document.getElementById('answer')
 
-  example.addEventListener("click", function () {
+  example.addEventListener("click", function() {
     fetch('https://jsonplaceholder.typicode.com/posts')
       .then(response => response.json())
       .then(array => {
@@ -14,13 +14,16 @@
       })
   })
 
-  cw1.addEventListener("click", function () {
+  cw1.addEventListener("click", function() {
+    answer.textContent = "Loading…";
+
     fetch('https://jsonplaceholder.typicode.com/posts', {
       method: 'GET'
     })
       .then(response => response.json())
       .then(data => {
-        const posts = data; 
+        const posts = data;
+
         const listItems = posts.map(post => {
           return `<li><strong>${post.title}</strong><br>${post.body}</li>`;
         });
@@ -34,11 +37,11 @@
       });
   });
 
-  cw2.addEventListener("click", function () {
+  cw2.addEventListener("click", function() {
     //TODO
   })
 
-  cw3.addEventListener("click", function () {
+  cw3.addEventListener("click", function() {
     //TODO
   })
 
